1bf4669acde1f7d7904c97b6cabc83fb69f82b54
GabiBarbera <gabriel.barberaa@gmail.com>
2023-12-28 14:18:56 -0300

    manejo jdbc

---
+++ src/main/java/datos/PersonaDAO.java
@@ -0,0 +1,49 @@
+package datos;
+
+import domain.Persona;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static datos.Conexion.*;
+
+public class PersonaDAO {
+    private static final String SQL_SELECT = "SELECT idpersona, nombre, apellido, email, telefono FROM persona";
+
+    public List<Persona> seleccionar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Persona persona = null;
+        List<Persona> personas = new ArrayList<>();
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int idPersona = rs.getInt("idpersona");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+
+                persona = new Persona(idPersona, nombre, apellido, email, telefono);
+
+                personas.add(persona);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexion.close(rs);
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException e) {
+                e.printStackTrace(System.out);
+            }
+        }
+        return personas;
+    }
+}
